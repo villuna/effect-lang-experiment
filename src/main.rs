fd44f7bee15d00bfe64cfbfbadd5648a4fc305bb
Luna Borella <ulfrthewolf@gmail.com>
2026-04-30 23:04:32 +1000

    Added basic command line args

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use std::fs::read_to_string;
 use crate::{interpreter::interpret, parse::parse};
 
 fn main() -> Result<()> {
-    let text = read_to_string("test.villi").context("File not found")?;
+    let Some(filename) = std::env::args().nth(1) else {
+        println!("Please pass a file to be interpreted");
+        return Ok(());
+    };
+
+    let text = read_to_string(filename).context("File not found")?;
     let program = parse(&text).context("Parse error")?;
 
-    dbg!(&program);
+    //dbg!(&program);
     interpret(&program);
 
     Ok(())
